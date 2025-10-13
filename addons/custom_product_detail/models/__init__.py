04ffe7f5153800f50f5b6028076b25bc7e4c2883
HAMZA0622 <hamzarafi0622@gmail.com>
2025-10-13 18:59:29 +0500

    ok

--- addons/custom_product_detail/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import product_template
-from . import product_attribute_value
-from . import product_attribute
-from . import website_menu
-from . import product_collection_set
-from . import product_template_attribute_value
--- addons/custom_product_detail/models/product_template_attribute_value.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-class ProductTemplateAttributeValue(models.Model):
-    _inherit = 'product.template.attribute.value'
-
-    image_1024 = fields.Image(related='product_attribute_value_id.image_1024')
-    image_512 = fields.Image(related='product_attribute_value_id.image_512')
-    image_256 = fields.Image(related='product_attribute_value_id.image_256')
